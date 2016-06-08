a70cdac973447cd829a6c18aa8f369a6a4aa0c0e
paf02 <pallito020390@hotmail.com>
2016-06-08 17:55:50 -0600

    a

--- frontend/app/scripts/main.js
+++ frontend/app/scripts/main.js
@@ -44,7 +44,7 @@ MyApp.angular.controller('appController', ['$scope', '$location', 'InitService',
 	$scope.auth = false;
 
 	DataService.getUsers(function(results) {
-		$scope.users = results.data.user;
+		$scope.users = results.data.User;
 		pagination();
 	}, function() {
 		console.log('fail'); 
@@ -83,59 +83,144 @@ MyApp.angular.controller('appController', ['$scope', '$location', 'InitService',
         return len;            
 	}
 
-	function getMondays() {
-      var d = new Date(),
-          month = d.getMonth(),
-          mondays = [],
-          count = 0;
+	// function getMondays() {
+ //      var d = new Date(),
+ //          month = d.getMonth(),
+ //          mondays = [],
+ //          count = 0;
 
-      d.setDate(1);
+ //      d.setDate(1);
 
-      // Get the first Monday in the month
-      while (d.getDay() !== 1) {
-          d.setDate(d.getDate() + 1);
-      }
+ //      // Get the first Monday in the month
+ //      while (d.getDay() !== 1) {
+ //          d.setDate(d.getDate() + 1);
+ //      }
 
-      // Get all the other Mondays in the month
-      while (d.getMonth() === month) {
-          mondays.push(new Date(d.getTime()));
-          d.setDate(d.getDate() + 7);
-          count++;
-          if(count>2) {
-          	break;
-          }
-      }
+ //      // Get all the other Mondays in the month
+ //      while (d.getMonth() === month) {
+ //          mondays.push(new Date(d.getTime()));
+ //          d.setDate(d.getDate() + 7);
+ //          count++;
+ //          if(count>2) {
+ //          	break;
+ //          }
+ //      }
 
-      return mondays;
-  	}
+ //      return mondays;
+ //  	}
   
-  	var m = getMondays();
-  	$scope.dates = [];
+ //  	var m = getMondays();
+ //  	$scope.dates = [];
   
-  	m.forEach(function(ele) {
-	    var x = new Date(ele);
-	    var y = x.getTime();
-	    var z, i, dates;
+ //  	m.forEach(function(ele) {
+	//     var x = new Date(ele);
+	//     var y = x.getTime();
+	//     var z, i, dates;
 	    
-	    for (i=0; i<=4; i++) {
-	    	z = (x.getDay() + i * 24 * 60 * 60 * 1000);
-	      dates = new Date(y+z);
-	      $scope.dates.push(dates);
-	    }  	
-	});
+	//     for (i=0; i<=4; i++) {
+	//     	z = (x.getDay() + i * 24 * 60 * 60 * 1000);
+	//       dates = new Date(y+z);
+	//       $scope.dates.push(dates);
+	//     }  	
+	// });
 
-  var days = [];
+  // var days = [];
+
+  $scope.dates = [];
 
   (function() {
-  	var t = new Date(),
-  			day = null;
+  	var startDate = '2016/05/04',
+  			today = new Date(startDate),
+  			day_mili = null,
+  			day = null,
+  			day_number = -1,
+  			i = 1,
+  			weeks = 0;
   	
-  	for (var i=1; i<104; i++){
-  		day =	new Date().setDate(t.getDate() + i);
-  		days.push(new Date(day));
+  	var day_numberOr = today.getDay();
+
+  	// 2 == 2 weeks
+  	while (weeks < 2){
+  		// get the day in millis
+  		day_mili =	new Date(startDate).setDate(today.getDate() + i);
+  		// parse the Date to a Date format
+  		day = new Date(day_mili);
+  		// get the day in the week
+  		day_number = day.getDay();
+  		if (i == 1) {
+  			calza(day_number);
+  		}
+
+  		if (day_number == day_numberOr) 
+  			weeks++;
+
+  		// filter saturdays and sundays
+  		if (0 < day_number && 6 > day_number)
+  			$scope.dates.push(day);
+  			
+			i++;
+			// console.log(day);
   	}
-  	console.log(days);
   })();
+
+  function calza(day_number) {
+  	for (var i = day_number - 1; i >= 1; i--) {
+  		$scope.dates.push('');
+  	};  
+  }
+  $scope.getBreakLine = function(dayView, indx) {
+  	// var yssss = '';
+
+  	if (dayView) {
+			if (indx == 0) {
+	  		return 'block';
+	  	} else {
+		  	if (dayView.getDay() > 1) {
+		  		return 'block';
+		  	} else {
+		  		return '';
+		  	}
+	  	}
+		} else {
+			return 'hid block';
+		}
+  } 
+
+  $scope.getCSSClass = function(user, dayView) {
+  	var day = null;
+  	var yssss = '';
+  	
+  	
+		for (var indx = 0; indx < user.calendarPoint.length; indx++) {
+  		day = new Date(user.calendarPoint[indx].date);
+  		// console.log(parseDate(day));
+  		// console.log(parseDate(dayView));
+
+  		if (parseDate(day) == parseDate(dayView)) {
+  			// console.log('match');
+  			yssss = 'book';
+  			break;
+  		}
+  	};
+  	// } else {
+  	// 	yssss = 'hid';
+  	// }
+
+
+  	
+
+
+  	// console.log(dayView.getFullYear() + "/" + (dayView.getMonth() + 1) + "/" + dayView.getDate());
+  	return yssss;
+  }   
+
+  function parseDate(day) {
+  	try {
+  		return day.getFullYear() + "/" + (day.getMonth() + 1) + "/" + day.getDate();
+  	} catch(e) {
+  		return '';
+  	}
+  }
 
 	$scope.credentials = {
 		username: "John Smith",
@@ -166,16 +251,6 @@ MyApp.angular.filter('startFrom', function() {
     }
 });
 
-MyApp.angular.directive('toggle', function(){
-  return {
-    restrict: 'A',
-    link: function(scope, element, attrs){
-      if (attrs.toggle=="tooltip"){
-        $(element).tooltip();
-      }
-    }
-  };
-});
 MyApp.angular.factory('DataService', ['$document', '$http', function ($document, $http) {
 	'use strict';
 
@@ -259,3 +334,13 @@ MyApp.angular.factory('InitService', ['$document', function ($document) {
   return pub;
   
 }]);
+MyApp.angular.directive('toggle', function(){
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs){
+      if (attrs.toggle=="tooltip"){
+        $(element).tooltip();
+      }
+    }
+  };
+});
